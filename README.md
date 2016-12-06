39e517959c4516d17f04d7ecde737521e4d242c7
Feng Yu <abcfy2@163.com>
2016-12-06 15:04:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Generate password
+
+A very simple shell script to generate random password. You can use `./gen_passwd.sh -h|--help` for help.
---
+++ gen_passwd.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+help() {
+    cat <<EOF
+$0 [-h|--help] [-w|--length <int>] [-l|--level 1-4]
+-h|--help      print this help
+-w|--length    length of password. DEFALUT: 8
+-l|--level     complexity level of password. DEFAULT: 3
+EOF
+}
+
+parse_arg() {
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                help
+                exit
+                ;;
+            -w|--length)
+                LENGTH="$2"
+                shift
+                ;;
+            -l|--level)
+                LEVEL="$2"
+                shift
+                ;;
+        esac
+        shift
+    done
+}
+
+gen_random_passwd() {
+    PASSWD_PATTERN='a-zA-Z0-9'
+    case $LEVEL in
+        1) PASSWD_PATTERN='0-9' ;;
+        2) PASSWD_PATTERN='a-z0-9' ;;
+        3) PASSWD_PATTERN='a-zA-Z0-9' ;;
+        4) PASSED_PATTERN='[[:graph:]]' ;;
+    esac
+
+    tr -dc "${PASSWD_PATTERN}" < /dev/urandom | fold -w "${LENGTH}" | head -1
+}
+
+declare -i LENGTH=8
+declare -i LEVEL=3
+parse_arg "$@"
+gen_random_passwd
